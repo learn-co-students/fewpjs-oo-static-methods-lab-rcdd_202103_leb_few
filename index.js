851b0e02418e6825d2a2363e790ff9bae8acbc7e
Salim Al Mersally <salim.almersally@lau.edu>
2021-05-08 09:29:47 +0000

    Done.

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ class Formatter {
     return string.charAt(0).toUpperCase() + string.slice(1);
   }
   static sanitize(string){
-    string = string.replace(/[^a-z0-9áéíóúñü' \.,_-]/gim,"");
+    string = string.replace(/[^a-z0-9áéíóúñü' -]/gim,"");
     return string.trim();
   }
   static titleize(string){
