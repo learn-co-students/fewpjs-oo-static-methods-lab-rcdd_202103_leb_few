a595050c5542c04f700da538bbe235057ff50b44
Nour baltaji <nourbaltaji99@gmail.com>
2021-05-08 09:54:21 +0000

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,24 @@
 class Formatter {
-  //add static methods here
+  static capitalize(string){
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
+  static sanitize(string){
+    return string.replace(/[^\w\s-']+/g, '');
+  }
+  static titleize(string){
+    const oldWords = string.split(" ");
+
+    const lowers = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from'];
+    let newWords = oldWords.map((word) => {
+        if(lowers.includes(word)){
+        return word[0] + word.substring(1);
+      }
+      else{
+        return word[0].toUpperCase() + word.substring(1);
+      }
+    });
+    newWords[0]= this.capitalize(newWords[0]);
+    let newString = newWords.join(" ");
+    return newString;
+  }
 }
