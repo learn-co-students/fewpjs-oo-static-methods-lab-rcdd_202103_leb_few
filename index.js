df5ab714288fecc2da90f6c9c1fe7945c394e7e8
Hazem El Sankari <hazemleb96@gmail.com>
2021-05-12 10:31:42 +0000

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
 class Formatter {
-  //add static methods here
+  static capitalize(str){
+    let new_str = str[0].toUpperCase() + str.substr(1,str.length);
+    return new_str
+  }
+  
+  
+  static sanitize(str){
+    return str.replace(/[^A-Za-z0-9-']+/g, '');
+  }
+  
+  titleize(string) {
+    let words = string.split(' ');
+    for (let n = 0; n < words.length; n++) {
+      words[n] = words[n].charAt(0).toUpperCase() + words[n].slice(1);
+    }
+    return words.join(' ');
+  }
 }
