f421c6796e4efb53669cd4e0dbd991aa1a581697
Hazem El Sankari <hazemleb96@gmail.com>
2021-05-12 10:57:56 +0000

    Done.

--- index.js
+++ index.js
@@ -6,13 +6,18 @@ class Formatter {
   
   
   static sanitize(str){
-    return str.replace(/[^A-Za-z0-9-']+/g, '');
+    return str.replace(/[^A-Za-z0-9-' ]+/g, '');
   }
   
-  titleize(string) {
+  static titleize(string) {
     let words = string.split(' ');
     for (let n = 0; n < words.length; n++) {
-      words[n] = words[n].charAt(0).toUpperCase() + words[n].slice(1);
+      if(words[n]!=="the" && words[n]!=="an"&& words[n]!=="but" && words[n]!=="of" && words[n]!=="for"&& words[n]!=="at"&& words[n]!=="by"&& words[n]!=="from"&& words[n]!=="and" && words[n]!=="a"){
+        words[n] = words[n].charAt(0).toUpperCase() + words[n].slice(1);
+      }
+      if(words[0]==="a"){
+        words[0] = words[0].charAt(0).toUpperCase() + words[n].slice(1);
+      }
     }
     return words.join(' ');
   }
